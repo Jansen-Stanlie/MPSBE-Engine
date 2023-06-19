b471388daec7da7a647bd32bad3209821d307eea
Oskar Putra <oskar.satriawan@pactindo.com>
2023-06-19 16:53:23 +0700

    update config file

--- util/util.go
+++ util/util.go
@@ -1,33 +1,33 @@
 package util
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"encoding/hex"
 	"fmt"
 	"github.com/Jansen-Stanlie/MPSBE-Engine/pkg/common/db"
 	"github.com/Jansen-Stanlie/MPSBE-Engine/pkg/common/models"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
+	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 	"time"
 )
 
-func LoadConfig() *models.Config {
-	//https://github.com/Jansen-Stanlie/MPSBE-Engine/blob/main/envs/.env
+func LoadConfig(r *gin.Engine) *models.Config {
+	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("current dir" + dir)
+
 	viper.SetConfigFile(os.Getenv("GOPATH") + "/pkg/mod/github.com/!jansen-!stanlie/!m!p!s!b!e-!engine@v0.0.0-20230619082206-7b41d38bfe13/envs/.env")
 	viper.ReadInConfig()
 
 	port := viper.Get("PORT").(string)
 
 	dbUrl := viper.Get("DB_URL").(string)
-	fmt.Println("port : " + port)
-	fmt.Println("dbUrl : " + dbUrl)
 
 	h := db.Init(dbUrl)
-
-	r := gin.Default()
 
 	s := &http.Server{
 		Addr:           port,
@@ -43,36 +43,4 @@ func LoadConfig() *models.Config {
 	}
 
 	return config
-}
-
-func decrypt(encryptedString string, keyString string) (decryptedString string) {
-
-	key, _ := hex.DecodeString(keyString)
-	enc, _ := hex.DecodeString(encryptedString)
-
-	//Create a new Cipher Block from the key
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	//Create a new GCM
-	aesGCM, err := cipher.NewGCM(block)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	//Get the nonce size
-	nonceSize := aesGCM.NonceSize()
-
-	//Extract the nonce from the encrypted data
-	nonce, ciphertext := enc[:nonceSize], enc[nonceSize:]
-
-	//Decrypt the data
-	plaintext, err := aesGCM.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	return fmt.Sprintf("%s", plaintext)
 }
